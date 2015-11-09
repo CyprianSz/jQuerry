2f2663f2eac5fd492e155019896e76ea4ecd8699
Cyprian <szwajka.cyprian@gmail.com>
2015-11-09 20:22:56 +0100

    update README

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ $(document).ready(function(){
 		for(i=1;i<=Math.pow(howmany,2);i++){
 			$('#container').append('<div class="grid"></div>');
 		}
+		$('.grid').mouseenter(function() { 
+		$(this).addClass('colored');
+	 });
+
 
 	});
 
